0d3682cb19ffa9ab03ed8f9f30327f57b92bfebd
andrejsloboda <sloboda.andrej@gmail.com>
2022-11-14 22:26:50 +0000

    Loging form plus session management

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,25 +1,36 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import session
 from .database import articles
 
 flask_app = Flask(__name__)
+flask_app.secret_key = b'\xb6\xdf\r]^\xd5s\x8aE^~\x1b\x9e\x7f\t\xac4\x91\xd7\x8d\xc3\xfb|\xde'
+
 
 @flask_app.route("/")
 def view_welcome_page():
     return render_template("welcome_page.jinja")
 
+
 @flask_app.route("/about/")
 def view_about():
     return render_template("about.jinja")
 
+
 @flask_app.route("/admin/")
 def view_admin():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
     return render_template("admin.jinja")
+
 
 @flask_app.route("/articles/")
 def view_articles():
     return render_template("articles.jinja", articles=articles.items())
+
 
 @flask_app.route("/articles/<int:art_id>")
 def view_article(art_id):
@@ -28,5 +39,26 @@ def view_article(art_id):
         return render_template("article.jinja", article=article)
     else: 
         return render_template("article_not_found.jinja", art_id=art_id)
+    
 
+@flask_app.route("/login/", methods=["GET"])
+def view_login():
+    return render_template("login.jinja")
+
+
+@flask_app.route("/login/", methods=["POST"])
+def view_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == "admin" and password == "admin":
+        session["logged"] = True
+        return redirect(url_for("view_admin"))
+    else:
+        return redirect(url_for("view_login"))
+    
+    
+@flask_app.route("/logout/", methods=["POST"])
+def logout_user():
+    session.pop("logged")
+    return redirect(url_for("view_welcome_page"))
 
