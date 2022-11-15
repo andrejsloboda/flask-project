1227930447b59db4027383500468b3def67c077c
andrejsloboda <sloboda.andrej@gmail.com>
2022-11-15 22:38:09 +0000

    integration of flash messages

--- mdblog/app.py
+++ mdblog/app.py
@@ -5,6 +5,7 @@ from flask import redirect
 from flask import url_for
 from flask import session
 from flask import g  # Main application context
+from flask import flash
 import sqlite3
 from .database import articles
 import os
@@ -29,6 +30,7 @@ def view_about():
 @flask_app.route("/admin/")
 def view_admin():
     if "logged" not in session:
+        flash("You must be logged in", "alert-danger")
         return redirect(url_for("view_login"))
     return render_template("admin.jinja")
 
@@ -47,6 +49,7 @@ def add_article():
     db.execute("insert into articles (title, content) values (?, ?)", 
         [request.form.get("title"), request.form.get("content")])
     db.commit()
+    flash("Article was saved")
     return redirect(url_for("view_articles"))
 
 
@@ -67,20 +70,23 @@ def view_login():
 
 
 @flask_app.route("/login/", methods=["POST"])
-def view_user():
+def login_user():
     username = request.form["username"]
     password = request.form["password"]
     if username == flask_app.config["USERNAME"] and \
             password == flask_app.config["PASSWORD"]:
         session["logged"] = True
+        flash("Login successful", "alert-success")
         return redirect(url_for("view_admin"))
     else:
+        flash("Invalid credentials", "alert-danger")
         return redirect(url_for("view_login"))
     
 
 @flask_app.route("/logout/", methods=["POST"])
 def logout_user():
     session.pop("logged")
+    flash("Logout successful", "alert-success")
     return redirect(url_for("view_welcome_page"))
 
 
