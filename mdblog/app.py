44b061a2959fadc1bc064c9ccea4832593970246
andrejsloboda <sloboda.andrej@gmail.com>
2022-11-17 14:04:57 +0000

    Updated app.py according to tutorial

--- mdblog/app.py
+++ mdblog/app.py
@@ -4,46 +4,45 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import session
-from flask import g  # Main application context
+from flask import g
 from flask import flash
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField
 from wtforms import PasswordField
+from wtforms import TextAreaField
 from wtforms.validators import InputRequired
 
+
 import sqlite3
-from .database import articles
 import os
 
+
 flask_app = Flask(__name__)
+
 flask_app.config.from_pyfile("/vagrant/configs/default.py")
 
 if "MDBLOG_CONFIG" in os.environ:
     flask_app.config.from_envvar("MDBLOG_CONFIG")
 
-    
 ## FORMS
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
 
-    
 class ArticleForm(FlaskForm):
     title = StringField("Title", validators=[InputRequired()])
     content = TextAreaField("Content")
 
-    
+
 ## CONTROLLERS
 @flask_app.route("/")
 def view_welcome_page():
     return render_template("welcome_page.jinja")
 
-
 @flask_app.route("/about/")
 def view_about():
     return render_template("about.jinja")
-
 
 @flask_app.route("/admin/")
 def view_admin():
@@ -52,9 +51,8 @@ def view_admin():
         return redirect(url_for("view_login"))
     return render_template("admin.jinja")
 
-
-### Articles
-@flask_app.route("/articles/")
+### ARTICLES
+@flask_app.route("/articles/", methods=["GET"])
 def view_articles():
     db = get_db()
     cur = db.execute("select * from articles order by id desc")
@@ -62,23 +60,41 @@ def view_articles():
     return render_template("articles.jinja", articles=articles)
 
 
-@flask_app.route("/articles/<int:art_id>")
+@flask_app.route("/articles/new/", methods=["GET"])
+def view_add_article():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
+
+    form = ArticleForm()
+    return render_template("article_editor.jinja", form=form)
+
+@flask_app.route("/articles/", methods=["POST"])
+def add_article():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
+
+    db = get_db()
+    db.execute("insert into articles (title, content) values (?, ?)",
+            [request.form.get("title"), request.form.get("content")])
+    db.commit()
+    flash("Article was saved", "alert-success")
+    return redirect(url_for("view_articles"))
+
+@flask_app.route("/articles/<int:art_id>/")
 def view_article(art_id):
     db = get_db()
-    cur = db.execute("select * from articles where id=(?)", [art_id])
+    cur = db.execute("select * from articles where id=(?)",[art_id])
     article = cur.fetchone()
     if article:
         return render_template("article.jinja", article=article)
-    else: 
-        return render_template("article_not_found.jinja", art_id=art_id)
-  
+    return render_template("article_not_found.jinja", art_id=art_id)
 
-@flask_app.route("/articles/<int:art_id>/edit", methods=["GET"])
+@flask_app.route("/articles/<int:art_id>/edit/", methods=["GET"])
 def view_article_editor(art_id):
     if "logged" not in session:
         return redirect(url_for("view_login"))
     db = get_db()
-    cur = db.execute("select * from articles where id=(?)", [art_id])
+    cur = db.execute("select * from articles where id=(?)",[art_id])
     article = cur.fetchone()
     if article:
         form = ArticleForm()
@@ -93,45 +109,25 @@ def edit_article(art_id):
     if "logged" not in session:
         return redirect(url_for("view_login"))
     db = get_db()
-    cur = db.execute("select * from articles where id=(?)", [art_id])
+    cur = db.execute("select * from articles where id=(?)",[art_id])
     article = cur.fetchone()
     if article:
         edit_form = ArticleForm(request.form)
         if edit_form.validate():
             db.execute("update articles set title=?, content=? where id=?",
-                [edit_form.title.data, edit_form.content.data, art_id])
+                    [edit_form.title.data, edit_form.content.data, art_id])
             db.commit()
             flash("Edit saved", "alert-success")
             return redirect(url_for("view_article", art_id=art_id))
         else:
-            for error in edit_form.errors:
+            for error in login_form.errors:
                 flash("{} is missing".format(error), "alert-danger")
             return redirect(url_for("view_login"))
-    
-
-@flask_app.route("/articles/new/", methods=["GET"])
-def view_add_article():
-    if "logged" not in session:
-        return redirect(url_for("view_login"))
-    form = ArticleForm()
-    return render_template("article_editor.jinja", form=form)
-    
-
-@flask_app.route("/articles/new", methods=["POST"])
-def add_article():
-    db = get_db()
-    db.execute("insert into articles (title, content) values(?, ?)", 
-        [request.form.get("title"), request.form.get("content")])
-    db.commit()
-    flash("Article was saved", "alert-success")
-    return redirect(url_for("view_articles"))
-
 
 @flask_app.route("/login/", methods=["GET"])
 def view_login():
     login_form = LoginForm()
     return render_template("login.jinja", form=login_form)
-
 
 @flask_app.route("/login/", methods=["POST"])
 def login_user():
@@ -150,7 +146,6 @@ def login_user():
             flash("{} is missing".format(error), "alert-danger")
         return redirect(url_for("view_login"))
 
-    
 @flask_app.route("/logout/", methods=["POST"])
 def logout_user():
     session.pop("logged")
@@ -164,31 +159,15 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
-
-# Lazy loader
 def get_db():
     if not hasattr(g, "sqlite_db"):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-
 @flask_app.teardown_appcontext
 def close_db(error):
     if hasattr(g, "sqlite_db"):
         g.sqlite_db.close()
-
-
-def get_db():
-    if not hasattr(g, "sqlite_db"):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@flask_app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, "sqlite_db"):
-        g.sqlite_db.close()
-
 
 def init_db(app):
     with app.app_context():
